c2eed80de424509dc38460b1d2fa4bcd4ef16939
David Miller <dmiller2117@gmail.com>
2021-09-23 15:24:03 +0100

    Adding PredicateAndConsumerExample

--- src/com/learnJava8/functionalInterfaces/PredicateAndConsumerExample.java
+++ src/com/learnJava8/functionalInterfaces/PredicateAndConsumerExample.java
@@ -5,6 +5,7 @@ import com.learnJava8.data.StudentDataBase;
 
 import java.util.List;
 import java.util.function.BiConsumer;
+import java.util.function.BiPredicate;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
@@ -12,22 +13,32 @@ public class PredicateAndConsumerExample {
 
     Predicate<Student> gradeLevelPredicate = (s) -> s.getGradeLevel() >= 3;
     Predicate<Student> gpaLevelPredicate = (s) -> s.getGpa() >= 3.9;
+    BiPredicate<Integer, Double> biPredicate = (gradeLevel, gpa) -> gradeLevel >= 3 && gpa >= 3.9;
 
     BiConsumer<String, List<String>> studentBiConsumer = (name, activities) -> System.out.println(name + " : " + activities);
 
     Consumer<Student> studentConsumer = (student -> {
-        if(gradeLevelPredicate.and(gpaLevelPredicate).test(student)){
+        if (gradeLevelPredicate.and(gpaLevelPredicate).test(student)) {
             studentBiConsumer.accept(student.getName(), student.getActivities());
         }
     });
 
-    private void printNameAndActivities(List<Student> students){
-        students.forEach(studentConsumer);
-    }
+    Consumer<Student> studentConsumerWithBiPredicate = (student -> {
+        if (biPredicate.test(student.getGradeLevel(), student.getGpa())) {
+            studentBiConsumer.accept(student.getName(), student.getActivities());
+        }
+    });
 
     public static void main(String[] args) {
         List<Student> studentList = StudentDataBase.getAllStudents();
         new PredicateAndConsumerExample().printNameAndActivities(studentList);
 
+    }
+
+    private void printNameAndActivities(List<Student> students) {
+        System.out.println("with two Predicates:");
+        students.forEach(studentConsumer);
+        System.out.println("with BiPredicate:");
+        students.forEach(studentConsumerWithBiPredicate);
     }
 }
