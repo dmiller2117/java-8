e3f3049421a3d81dace7d4dfb5fbe278e7affd4a
David Miller <dmiller2117@gmail.com>
2021-10-06 13:50:36 +0100

    Adding a function method reference example

--- src/com/learnJava8/methodReference/FunctionMethodReferenceExample.java
+++ src/com/learnJava8/methodReference/FunctionMethodReferenceExample.java
@@ -1,5 +1,15 @@
 package com.learnJava8.methodReference;
 
+import java.util.function.Function;
+
 public class FunctionMethodReferenceExample {
 
+    @SuppressWarnings("Convert2MethodRef")
+    static Function<String, String> toUpperCaseLambda = (s) -> s.toUpperCase();
+    static Function<String, String> toUpperCaseMethodReference = String::toUpperCase;
+
+    public static void main(String[] args) {
+        System.out.println(toUpperCaseLambda.apply("java8"));
+        System.out.println(toUpperCaseMethodReference.apply("java8"));
+    }
 }
